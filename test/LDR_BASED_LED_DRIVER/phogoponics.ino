9bd804aac455b1ce2f863a2d9c3f9ff11ac51a49
sonus <sonusubi456@gmail.com>
2025-01-25 14:16:00 +0530

    drivercode

---
+++ test/LDR_BASED_LED_DRIVER/phogoponics.ino
@@ -0,0 +1,15 @@
+#define LDR 33
+#define DRIVER 32
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LDR,INPUT);
+pinMode(DRIVER,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int analogValue = analogRead(LDR);
+int pwm = map(analogValue,0,4095,0,255);
+analogWrite(DRIVER,pwm);
+delay(100);
+}
